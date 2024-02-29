7d9861feac72e4c1d8cc8b92e24a232a36be020d
LeanneBoro <leanneboro@gmail.com>
2024-02-29 11:36:36 +0200

    Make line change work

--- js/memecontroller.js
+++ js/memecontroller.js
@@ -4,23 +4,22 @@ let gElCanvas
 let gCtx
 
 function onInit() {
-    console.log('yas yas yas')
     gElCanvas = document.querySelector('canvas')
     gCtx = gElCanvas.getContext('2d')
 
     renderMeme()
-    drawText('Hello', 232, 52)
 
 }
 
 function renderMeme() {
     let meme = getMeme()
     let line = meme.lines
+    let lineIdx = meme.selectedLineIdx
     var img = new Image()
     img.src = `img/${meme.selectedImgId}.jpg`
     img.onload = () => {
         gCtx.drawImage(img, 0, 0)
-        drawText(line[0].txt, 200, 60)
+        drawText(line[lineIdx].txt,200, 30)
     }
 }
 
@@ -38,3 +37,9 @@ function drawText(text, x, y) {
     gCtx.strokeText(text, x, y)
 }
 
+function onEnterText({ value }) {
+    let meme = getMeme()
+    let lineIdx = meme.selectedLineIdx
+    meme.lines[lineIdx].txt = value
+    renderMeme()
+}
